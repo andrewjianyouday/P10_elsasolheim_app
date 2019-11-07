ae090e6116143d4504732d8e8487c08599f2740c
H151699 <andrewjianyouday@gmail.com>
2019-11-08 00:55:48 +0100

    rest of files

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:elsasolheim/screens/splashscreen.dart';
+import 'package:elsasolheim/screens/splashscreen3.dart';
+
+import 'package:flutter/material.dart';
+import 'package:elsasolheim/screens/homescreen.dart';
+import 'package:elsasolheim/screens/dashboardscreen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          body1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      initialRoute: SplashScreen.id,
+      routes: {
+        SplashScreen.id: (context) => SplashScreen(),
+//        HomeScreen.id: (context) => DashboardScreen(),
+      },
+    );
+  }
+}
